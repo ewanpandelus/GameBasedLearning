cbdb39d95e3e7d610a334ecd9900ade5ac298849
Ewan Pandelus <ewanpandelus@hotmail.com>
2021-01-12 14:30:25 +0000

    Refactoring Code
    Refactoring LevelLoader.cs as the code was very inefficient.

--- GameBasedLearing/Assets/Scripts/LevelLoader.cs
+++ GameBasedLearing/Assets/Scripts/LevelLoader.cs
@@ -9,15 +9,11 @@ public class LevelLoader : MonoBehaviour
     public string levelName;
     AudioManager AudioManagement;
 
-    private void Awake()
+    private void Start()
     {
         AudioManagement = AudioManager.instance;
     }
-    public void LoadGraph()
-    {
-        dynamicUI.FadeOutWinningPathtext();
-        StartCoroutine(LoadChooseGraphScene());
-    }
+
     public void LoadLevel()
     {
         StartCoroutine(LoadLevel(levelName));
@@ -29,60 +25,7 @@ public class LevelLoader : MonoBehaviour
         SceneManager.LoadScene(levelName);
     }
 
-    public void LoadEasyTSPButton()
-    {
-        StartCoroutine(LoadEasyTSP());
-    }
-    public void LoadNQueens2Button()
-    {
-        StartCoroutine(LoadNQueensLevel2());
-    }
-    public void LoadNQueens1Button()
-    {
-        StartCoroutine(LoadNQueensLevel1());
-    }
-    public void LoadBubbleSortButton()
-    {
-        StartCoroutine(LoadBubbleSortScene());
-    }
-    private IEnumerator LoadBubbleSortScene()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSecondsRealtime(1f);
-        SceneManager.LoadScene("BubbleSort");
-    }
-    private IEnumerator LoadChooseGraphScene()
-    { 
-    transition.SetTrigger("Start");
-    yield return new WaitForSecondsRealtime(1f);
-    SceneManager.LoadScene("ComplexityGraph");
-    }
-    private IEnumerator LoadEasyTSP()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSecondsRealtime(1f);
-        SceneManager.LoadScene("EasyTSP");
-    }
-    private IEnumerator LoadHardTSP()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSecondsRealtime(1f);
-        SceneManager.LoadScene("HardTSP");
-    }
 
-    private IEnumerator LoadNQueensLevel2()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSecondsRealtime(1f);
-        SceneManager.LoadScene("NQueensLevel2");
-    }
-
-    private IEnumerator LoadNQueensLevel1()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSecondsRealtime(1f);
-        SceneManager.LoadScene("NQueensLevel1");
-    }
 
     public void PlaySceneChangeSound()
     {
