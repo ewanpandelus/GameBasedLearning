af0c0070c1e153fd456c04fdf110edfbebcea744
Ewan Pandelus <ewanpandelus@hotmail.com>
2021-01-20 17:46:16 +0000

    Adding jumping on ladder functionality
    Adding jumpingwhen on lladders so that players can jump between ladders.

--- GameBasedLearing/Assets/Scripts/Ladder.cs
+++ GameBasedLearing/Assets/Scripts/Ladder.cs
@@ -16,10 +16,27 @@ public class Ladder : MonoBehaviour
         if (col.CompareTag("Player"))
         {
             playerMovement.SetIsClimbing(true);
-
+      
         }
     }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (playerMovement.GetVerticalVelocity() < 0.01 && playerMovement.GetIsJumping() == true)
+        {
+            playerMovement.SetIsClimbing(true);
+            playerMovement.SetIsJumping(false);
 
+        }
+        if (playerMovement.GetVerticalVelocity() > 0.01 && playerMovement.GetIsJumping() == true)
+        {
+            playerMovement.SetIsClimbing(false);
+        }
+        else
+        {
+            playerMovement.SetIsClimbing(true);
+        }
+
+    }
     void OnTriggerExit2D(Collider2D col)
     {
 
--- GameBasedLearing/Assets/Scripts/PlayerMovement.cs
+++ GameBasedLearing/Assets/Scripts/PlayerMovement.cs
@@ -13,18 +13,26 @@ public class PlayerMovement : MonoBehaviour
     private bool isClimbing = false;
     private float verticalVelocity = 0f;
     float verticalMove = 0f;
+    private bool isJumping = false;
+ 
 
     // Update is called once per frame
     void Update()
     {
         animator.speed = 1f;
+
+        if (isJumping)
+        {
+            verticalVelocity = rb.velocity.y;
+            horizontalVelocity = 12f;
+        }
         if (!isClimbing)
         {
             verticalVelocity = 0f;
             horizontalVelocity = 12f;
             NonClimbingMovement();
         }
-        else
+        if(!isJumping&&isClimbing)
         {
             verticalVelocity = 9f;
             horizontalVelocity = 3f; 
@@ -36,22 +44,63 @@ public class PlayerMovement : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal") * horizontalVelocity;
         verticalMove = Input.GetAxisRaw("Vertical") * verticalVelocity;
         animator.SetBool("IsJumping", false);
-        animator.SetBool("IsClimbing", true);
+        animator.SetBool("IsClimbing", isClimbing);
         if (verticalMove < 0.01)
         {
             animator.speed = 0f;
         }
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.speed = 1f;
+            
+            Jump();
+           
+        }
+        FlipCharacter();
     }
+
     private void NonClimbingMovement()
     {
         
         animator.SetBool("IsClimbing", false);
         horizontalMove = Input.GetAxisRaw("Horizontal") * horizontalVelocity;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
+            animator.speed = 1f;
+
             Jump();
+
         }
+        FlipCharacter();
+
+    }
+    private void FixedUpdate()
+    {
+        if (!isClimbing)
+        {
+            rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
+        }
+        else
+        {
+             rb.velocity = new Vector2(horizontalMove, verticalMove);
+        }
+    }
+    private void Jump()
+    {
+
+        if (isgrounded == true||isClimbing == true)
+        {
+            isClimbing = false;
+            isJumping = true;
+            animator.SetBool("IsClimbing", false);
+            animator.SetBool("IsJumping", true);
+            rb.velocity = new Vector2(rb.velocity.x, 17.5f);
+        }
+    }
+    private void FlipCharacter()
+    {
         if (horizontalMove > 0 && !m_FacingRight)
         {
             // ... flip the player.
@@ -62,26 +111,6 @@ public class PlayerMovement : MonoBehaviour
         {
             // ... flip the player.
             Flip();
-        }
-    }
-    private void FixedUpdate()
-    {
-        if (!isClimbing)
-        {
-            rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = new Vector2(horizontalMove, verticalMove);
-        }
-    }
-    private void Jump()
-    {
-
-        if (isgrounded == true)
-        {
-            animator.SetBool("IsJumping", true);
-            rb.velocity = new Vector2(rb.velocity.x, 17.5f);
         }
     }
     private void Flip()
@@ -97,18 +126,32 @@ public class PlayerMovement : MonoBehaviour
     //make sure u replace "floor" with your gameobject name.on which player is standing
     void OnCollisionEnter2D(Collision2D theCollision)
     {
-        if (theCollision.gameObject.transform.tag == "floor")
+        if (theCollision.gameObject.transform.tag == "floor"&&theCollision.gameObject.name!="Ladder")
         {
             
             isgrounded = true;
             animator.SetBool("IsJumping", false);
+            isJumping = false;
         }
     }
 
     void OnCollisionExit2D(Collision2D theCollision)
-        {
-         isgrounded = false;
-        }
+    {
+       isgrounded = false;
+    }
+    public float GetVerticalVelocity()
+    {
+      return this.rb.velocity.y;
+        
+    }
+    public bool GetIsJumping()
+    {
+        return this.isJumping;
+    }
+    public void SetIsJumping(bool _isJumping)
+    {
+        isJumping = _isJumping;
+    }
     public void SetIsClimbing(bool _isClimbing)
     {
         isClimbing = _isClimbing;
